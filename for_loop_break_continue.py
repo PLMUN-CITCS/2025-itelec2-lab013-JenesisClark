476b4d36ac55d3c42ce5e246676f9a3bd782047c
JenesisClark <Jenesisinfante252@gmail.com>
2025-02-17 21:55:48 +0800

    Submitting Python Week 04 - Laboratory # 13

--- for_loop_break_continue.py
+++ for_loop_break_continue.py
@@ -1,8 +1,8 @@
-numbers = list(range(1, 10))
+numbers = list(range(1, 11))
 
 for num in numbers:
-    if num == 1:
+    if num == 3:
         continue
-	if num == 10:
+	if num == 7:
         break  
 	print(num)
